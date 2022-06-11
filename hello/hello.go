8dacbf7a899eee09dae4ffdbf40babec3e097224
luhuiting <862469939@qq.com>
2022-06-11 23:38:27 +0800

    add branch dev

--- hello/hello.go
+++ hello/hello.go
@@ -4,11 +4,6 @@ import "fmt"
 
 func SayHi(name string) {
 	fmt.Printf("Hi!\n")
-	fmt.Printf("Here is a new branch dev!\n")
+	fmt.Printf("Here is a new branch: dev!\n")
 	fmt.Printf("Hello %s 我是鱼蛋 Nice to meet u\n", name)
-<<<<<<< HEAD
-	fmt.Printf("Creating a new branch is quick and simple.\n")
-=======
-	fmt.Printf("Creating a new branch is quick and simple.\n")
->>>>>>> feature1
 }
