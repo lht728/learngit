b876d51826fa7a5c99b18671fdc6c59061169b7a
luhuiting <862469939@qq.com>
2022-06-11 23:05:34 +0800

    quick and simple

--- hello/hello.go
+++ hello/hello.go
@@ -6,4 +6,5 @@ func SayHi(name string) {
 	fmt.Printf("Hi!\n")
 	fmt.Printf("Here is a new branch dev!\n")
 	fmt.Printf("Hello %s 我是鱼蛋 Nice to meet u\n", name)
+	fmt.Printf("Creating a new branch is quick AND simple.\n")
 }
